e15292bdc4c8a7d69cd886b0f188cf42e28152d9
navarretesofia <snavarrets@uc.cl>
2025-11-25 21:36:13 +0100

    mar25.11

--- projects/01MainAssignments/project1/script.js
+++ projects/01MainAssignments/project1/script.js
@@ -1,29 +1,69 @@
-let len = 10;
-let bg;
+
+let letras = ["F", "A", "S", "T", "E", "R",]
+let spacing = 100;
+let amplitude = 60;
+let speed = 0.05;
+let phaseOffset = 1;
+let letterSize=50;
+
 
 function setup() {
 	createCanvas(windowWidth,windowHeight);
-	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(10, 200, 58);
+	rectMode (CENTER);
+	textFont("Silkscreen");
+	textAlign(CENTER,CENTER);	
 }
 
 function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(20,250, 40);
-	
-	rect(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-	rect(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(89, 30, 33);
+	background("black");
+
+
+	if (amplitude>300) {
+		fill("red");
+		textSize(400);
+
+let shakeX = random(-2,2);
+let shakeY = random(-2,2);
+
+
+		text("STOP", width/2+shakeX, height/2+shakeY);
+		return;
+
+
 	}
-}
+
+
+
+	fill("blue")
+
+	let totalWidth = (letras.length -1)* spacing;
+	let startX = (width - totalWidth)/2;
+
+	for (let i=0; i<letras.length; i++) {
+
+		let x= startX+ i * spacing; 
+
+		let y = height/2 + sin(frameCount * speed + i * phaseOffset) * amplitude;
+	
+		fill("white");
+		textSize(letterSize)
+		text(letras[i], x, y);
+		
+	}
+
+	function windowResized() {
+		resizeCanvas(windowWidth, windowHeight);
+	}
+
+
+	}
+
+		function mousePressed() { 
+		amplitude+=10;
+		speed+=0.05;
+		spacing+=10;
+		letterSize+=10;
+
+	}
+
 
