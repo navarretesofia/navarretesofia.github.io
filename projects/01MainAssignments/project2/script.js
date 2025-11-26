1296b990b5390d87de9d5b6b76667bec71781349
navarretesofia <snavarrets@uc.cl>
2025-11-26 19:55:13 +0100

    update!

--- projects/01MainAssignments/project2/script.js
+++ projects/01MainAssignments/project2/script.js
@@ -1,29 +1,89 @@
-let len = 10;
-let bg;
+let digits = [];
 
 function setup() {
-	createCanvas(windowWidth,windowHeight);
-	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(200, 0, 58);
+  createCanvas(windowWidth,windowHeight);
+  rectMode(CORNER);
+  	textFont("Quantico");
+
+
+
+
+  let startDay = 50;  
+  let startMonth = 400;
+  let startYear = 750;
+  let gap = 140;    
+
+  for (let i = 0; i < 2; i++) {
+    digits.push(createDigit(startDay + i * gap, 50));
+  }
+
+ for (let i = 0; i < 2; i++) {
+    digits.push(createDigit(startMonth + i * gap, 50));
+  }
+
+   for (let i = 0; i < 4; i++) {
+    digits.push(createDigit(startYear + i * gap, 50));
+  }
+
 }
 
-function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(0,130, 140);
-	
-	ellipse(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-  	ellipse(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(189, 3, 33);
-	}
+function draw() {
+  background(10);
+
+  fill(200);
+  textSize(60);
+  text("DAY", 70, 100);
+  text("MONTH", 430, 100);
+  text("YEAR", 780, 100);
+
+  // Dibujar todos los dígitos
+  for (let d of digits) {
+    drawDigit(d);
+  }
+
+  noStroke();
+  rect(365, 390, 20, 20,4);
+  rect(715, 390, 20, 20,4);
 }
 
+
+function createDigit(offsetX, offsetY) {
+  return [
+    { name:"A", x: offsetX+40,  y: offsetY+100,     w: 80, h:20, on:false },
+    { name:"B", x: offsetX+120, y: offsetY+120,  w:20, h:100, on:false },
+    { name:"C", x: offsetX+120, y: offsetY+240, w:20, h:100, on:false },
+    { name:"D", x: offsetX+40,  y: offsetY+340, w:80, h:20, on:false },
+    { name:"E", x: offsetX+20,  y: offsetY+240, w:20, h:100, on:false },
+    { name:"F", x: offsetX+20,  y: offsetY+120,  w:20, h:100, on:false },
+    { name:"G", x: offsetX+40,  y: offsetY+220, w:80, h:20, on:false }
+  ];
+}
+
+
+
+function drawDigit(digit) {
+  for (let s of digit) {
+    if (s.on) fill(255);  
+    else fill(60);              
+
+    rect(s.x, s.y, s.w, s.h, 5);
+  }
+}
+
+
+
+
+function mousePressed() {
+  for (let digit of digits) {
+    for (let s of digit) {
+      if (
+        mouseX > s.x &&
+        mouseX < s.x + s.w &&
+        mouseY > s.y &&
+        mouseY < s.y + s.h
+      ) {
+        s.on = !s.on;
+      }
+    }
+  }
+}
